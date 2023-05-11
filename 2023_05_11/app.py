5db5432004800a52a04d2df19bba6436d395fb7d
Aaron <aaronchan289@gmail.com>
2023-05-11 10:13:51 +0800

    modify

---
+++ 2023_05_11/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+
+import pandas_datareader.data as pdr
+import yfinance as yf
+import datasource
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    stock_data = datasource.get_stock_data(stockid=2303)
+    return render_template("index.jinja.html", data=stock_data)
+
+
+@app.route("/features/")
+def features():
+
+    return render_template("features.jinja.html")
+
+
+@app.route("/pricing/")
+def priceing():
+
+    return render_template("pricing.jinja.html")
+
+
+@app.route("/about/")
+def about():
+    return render_template("about.jinja.html")
+
+
+@app.route("/form/",methods=['GET', 'POST'])
+def form():
+
+    return render_template("form.jinja.html")
