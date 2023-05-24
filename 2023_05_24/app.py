da43a2284a76182c2e3a125ce8ff60d3d2d4d29e
Aaron <aaronchan289@gmail.com>
2023-05-24 16:52:04 +0800

    modify

---
+++ 2023_05_24/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+import plotly.express as px
+import plotly
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    df = px.data.tips()
+    fig = px.scatter(df, x="total_bill", y="tip", trendline="ols")
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('index.jinja.html', graphJSON=graphJSON)
+
+@app.route("/index1")
+def index1():
+    df = px.data.tips()
+    fig = px.scatter(df, x="total_bill", y="tip", trendline="ols")
+    graphJSON = json.dumps(fig,cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('index1.jinja.html',graphJSON=graphJSON)
+
+@app.route("/index2")
+def index2():
+    df = px.data.tips()
+    fig = px.scatter(df, x="total_bill", y="tip", trendline="ols")
+    graphJSON = json.dumps(fig,cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('index2.jinja.html',graphJSON=graphJSON)
