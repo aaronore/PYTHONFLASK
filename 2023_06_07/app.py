16004dc1cbe210e7d1f96e11ab705bd57c9bd5c3
Aaron <aaronchan289@gmail.com>
2023-06-07 14:03:31 +0800

    modify

--- 2023_06_07/app.py
+++ 2023_06_07/app.py
@@ -1,10 +1,8 @@
-from  flask import Flask,request
+from  flask import Flask,render_template
 
 app = Flask(__name__)
 @app.route("/")
 def index():
-    print(request.args.get('name'))
-    print(request.args.get('name1'))
-    print(request.args.get('name2'))
-    return "Hello! World",200  #200static code狀態對就200不寫也預設200
+    # 200static code狀態對就200不寫也預設200
+    return render_template('index.jinja.html'), 200
 
