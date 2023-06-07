64fa5b27283bf6521ee48cba224c40085253e262
Aaron <aaronchan289@gmail.com>
2023-06-07 14:07:08 +0800

    modify

--- 2023_06_07/app.py
+++ 2023_06_07/app.py
@@ -3,6 +3,9 @@ from  flask import Flask,render_template
 app = Flask(__name__)
 @app.route("/")
 def index():
-    # 200static code狀態對就200不寫也預設200
-    return render_template('index.jinja.html'), 200
+    return render_template('index.jinja.html'), 200   # 200static code狀態對就200不寫也預設200
 
+
+@app.route("/login")
+def login():
+    return render_template('login.jinja.html')
