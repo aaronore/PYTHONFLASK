73605202bd0b72a7d5c2e1e3effe47ff61585a86
Aaron <aaronchan289@gmail.com>
2023-06-07 14:48:57 +0800

    modify

--- 2023_06_07/app.py
+++ 2023_06_07/app.py
@@ -6,6 +6,6 @@ def index():
     return render_template('index.jinja.html'), 200   # 200static code狀態對就200不寫也預設200
 
 
-@app.route("/login")
+@app.route("/login",methods=['GET', 'POST'])   
 def login():
     return render_template('login.jinja.html')
